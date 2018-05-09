dd77c485bfaab2c33fddb66de2729c4e88b82a61
Aaron <x14715335@student.ncirl.ie>
2018-05-09 00:23:02 +0000

    Renamed vendot to v in index

--- index.php
+++ index.php
@@ -17,15 +17,15 @@
   <title>CyBly</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <link href="v/bootstrap/css/bootstrap.css" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href="v/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 
   <!-- Plugin CSS -->
-  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="v/magnific-popup/magnific-popup.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
   <link href="css/creative.css" rel="stylesheet">
@@ -302,13 +302,13 @@
   </section>
 
   <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.js"></script>
+  <script src="v/jquery/jquery.js"></script>
+  <script src="v/bootstrap/js/bootstrap.bundle.js"></script>
 
   <!-- Plugin JavaScript -->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
-  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+  <script src="v/jquery-easing/jquery.easing.min.js"></script>
+  <script src="v/scrollreveal/scrollreveal.min.js"></script>
+  <script src="v/magnific-popup/jquery.magnific-popup.min.js"></script>
 
   <!-- Custom js/jquery scripts-->
   <script src="js/creative.js"></script>
